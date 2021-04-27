a180bda532b2e1a0bc138176d16cf210cbf1c47a
Azhia Harris <azhia_h@yahoo.com>
2021-04-26 20:00:43 -0700

    sign up screen frontend

--- Nfty/frontend/views/screens/SignUpScreen.js
+++ Nfty/frontend/views/screens/SignUpScreen.js
@@ -1,32 +1,184 @@
 import React, { Component } from 'react';
-import {ActivityIndicator, View, ScrollView, StyleSheet, Image, Text, Button, TouchableOpacity} from 'react-native';
+import {ActivityIndicator, View, ScrollView, StyleSheet, Image, Text, TextInput, Keyboard, KeyboardAvoidingView, TouchableOpacity} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import InstagramLogin from 'react-native-instagram-login';
 import {globalStyle} from "./global-style";
+
+{/* <InstagramLogin
+	ref={ref => (this.instagramLogin = ref)}
+	clientId='931cca1d0c154de3aafd83300ff8b288'
+	redirectUrl='https://google.com'
+	scopes={['public_content+follower_list']}
+	onLoginSuccess={(token) => this.setState({ token })}
+	onLoginFailure={(data) => this.setState({ failure: data })}
+	/> */}
 
 class SignUpScreen extends Component {
 	constructor(props){
 		super(props);
+		this.state = {
+			userName: '',
+			userEmail: "",
+			password: "", 
+			loading: false, 
+			errortext: "", 
+			registrationSuccess: false
+		};
+		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
-	// TO DO: add sign up functionality and save to storage, add facebook and instagram login
+	handleSubmit = () => {
+		this.setState({ errortext: '' })
+		if (!this.state.userEmail) {
+			alert('Please fill Email');
+			return;
+		  }
+		if (!this.state.userName) {
+		  alert('Please fill Name');
+		  return;
+		}
+		if (!this.state.userPassword) {
+		  alert('Please fill Password');
+		  return;
+		}
+		setLoading(true);
+		var dataToSend = {
+		  name: this.state.userName,
+		  email: this.state.userEmail,
+		  password: this.state.password,
+		};
+		// send to firebase
+	
+	};
+
     render() {
-        return (
-            <View >
+		if (this.state.registrationSuccess == true) {
+			return (
+			  <View
+				style={{
+				  flex: 1,
+				  backgroundColor: '#307ecc',
+				  justifyContent: 'center',
+				}}>
+				<Image
+				  source={require('../img/success.png')}
+				  style={{
+					width: '50%',
+					height: 100,
+					resizeMode: 'contain',
+					alignSelf: 'center',
+					margin: 30,
+				  }}
+				/>
+				<Text style={styles.successTextStyle}>
+				  Sign Up Successful!
+				</Text>
 				<TouchableOpacity
-                    style={globalStyle.buttonStyle}
-                    activeOpacity={0.5}
-                    onPress = {()=> this.props.navigation.navigate('WelcomeScreen')}
-                    >
-                    <Text style={globalStyle.buttonTextStyle}>Home</Text>
-                  </TouchableOpacity>
-				  <Text
+				  style={globalStyle.buttonStyle}
+				  activeOpacity={0.5}
+				  onPress={() => this.props.navigation.navigate('LoginScreen')}>
+				  <Text style={globalStyle.buttonTextStyle}>Login Now</Text>
+				</TouchableOpacity>
+			  </View>
+			);
+		}
+        return (
+			<View style={globalStyle.mainBody}>
+				{this.state.loading  &&
+					<ActivityIndicator
+					animating={this.state.loading}
+					color="#000000"
+					size="large"
+					style={styles.activityIndicator}
+					/> }
+				<ScrollView
+					keyboardShouldPersistTaps="handled"
+					contentContainerStyle={{
+					justifyContent: 'center',
+					alignContent: 'center',
+					}}>
+					<View style={{alignItems: 'center'}}>
+					<Image
+						source={require('../img/nfty_logo.png')}
+						style={{
+						width: '50%',
+						height: 100,
+						resizeMode: 'contain',
+						margin: 30,
+						}}
+					/>
+					</View>
+					<KeyboardAvoidingView enabled>
+					<View style={globalStyle.SectionStyle}>
+						<TextInput
+						style={globalStyle.inputStyle}
+						onChangeText={text => this.setState({ userName: text })}
+						underlineColorAndroid="#f000"
+						placeholder="Enter Name"
+						placeholderTextColor="#8b9cb5"
+						autoCapitalize="sentences"
+						returnKeyType="next"
+						blurOnSubmit={false}
+						/>
+					</View>
+					<View style={globalStyle.SectionStyle}>
+						<TextInput
+						style={globalStyle.inputStyle}
+						onChangeText={text => this.setState({ userEmail: text })}
+						underlineColorAndroid="#f000"
+						placeholder="Enter Email"
+						placeholderTextColor="#8b9cb5"
+						keyboardType="email-address"
+						returnKeyType="next"
+						blurOnSubmit={false}
+						/>
+					</View>
+					<View style={globalStyle.SectionStyle}>
+						<TextInput
+						style={globalStyle.inputStyle}
+						onChangeText={text => this.setState({ password: text })}
+						underlineColorAndroid="#f000"
+						placeholder="Enter Password"
+						placeholderTextColor="#8b9cb5"
+						returnKeyType="next"
+						secureTextEntry={true}
+						blurOnSubmit={false}
+						/>
+					</View>
+					{this.state.errortext != '' ? (
+						<Text style={globalStyle.errorTextStyle}>
+						{this.state.errortext}
+						</Text>
+					) : null}
+					<TouchableOpacity
+						style={globalStyle.buttonStyle}
+						activeOpacity={0.5}
+						onPress={this.handleSubmit}>
+						<Text style={globalStyle.buttonTextStyle}>SIGN UP</Text>
+					</TouchableOpacity>
+					<Text
                     style={globalStyle.registerTextStyle}
                     onPress={() => this.props.navigation.navigate('LoginScreen')}>
                     Have an account? Login
                   </Text>
-			</View>
+					</KeyboardAvoidingView>
+				</ScrollView>
+				</View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+	activityIndicator: {
+	  alignItems: 'center',
+	  height: 80,
+	},
+	successTextStyle: {
+		color: 'white',
+		textAlign: 'center',
+		fontSize: 18,
+		padding: 30,
+	  },
+  });
 
 export default SignUpScreen;
