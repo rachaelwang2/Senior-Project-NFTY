dee87b3a435dc065ca2621a4d4dabca6626f9bb4
Azhia Harris <azhia_h@yahoo.com>
2021-05-12 01:23:34 -0700

    add signup log

--- Nfty/frontend/redux/actions/ActionCreators.js
+++ Nfty/frontend/redux/actions/ActionCreators.js
@@ -131,6 +131,7 @@ export const signupUser = (email, password, fullname) => (dispatch) => {
 			.doc(user_uid)
 			.get()
 			.then(function(user) {
+				console.log("user successfully signed up")
 				dispatch(signupSuccess());
 			})
 			.catch(function(error) {
