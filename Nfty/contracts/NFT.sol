8f31fa0ef9dc765d900b828d48424fbb777bda30
Julien Brinson <jbrinson@stanford.edu>
2021-05-14 03:06:02 -0400

    integrating bare bones blockchain

--- Nfty/contracts/NFT.sol
+++ Nfty/contracts/NFT.sol
@@ -1,25 +1,25 @@
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract NFTSimple is ERC721 {
-	uint public nextTokenId;
-	address public admin;
+contract NFT is ERC721 {
+  uint public nextTokenId;
+  address public admin;
 
-	constructor() ERC721('Nfty', 'NFT') {
-		admin = msg.sender;
-	}
+  constructor() ERC721('My NFT', 'NFT') {
+    admin = msg.sender;
+  }
 
-	VRFCOonsumerBase(_VRFCoordinator, _LinkToken)
-	ERC721()
-	function mint(address to) external {
-		require(msg.sender == admin, 'only admin'):
-		_safeMint(to, nextTokenId);
-		nextTokenId++;
-	}
+  function mint(address to) external {
+    require(msg.sender == admin, 'only admin');
+    _safeMint(to, nextTokenId);
+    nextTokenId++;
+  }
 
-	function _baseURI() internal view override returns (string memory) {
-		return "http://0.0.0.0:8545/";
-	}
+  function _baseURI() internal view override returns (string memory) {
+    return '';
+  }
+
 }
