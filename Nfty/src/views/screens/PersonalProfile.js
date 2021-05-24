bf5e71e38a3c9322c0b01e375c5e8eb76f20014a
Rachael Wang <rachaelwang2@gmail.com>
2021-05-23 20:41:38 -0700

    frontend with backend incorporated

--- Nfty/src/views/screens/PersonalProfile.js
+++ Nfty/src/views/screens/PersonalProfile.js
@@ -2,19 +2,81 @@ import React, { Component } from 'react';
 import {ActivityIndicator, SafeAreaView, View, StyleSheet, Image, Text, Button, TouchableOpacity} from 'react-native';
 // import { connect } from "react-redux";
 // import {globalStyle, AppStyles} from "./global-style";
-// import {uploadImage, getUploadedImages} from "../../redux/actions/ActionCreators";
+import {uploadImage, getUploadedImages} from "../../redux/actions/ActionCreators";
 // import { Svg } from 'react-native-svg'
 
+const mapDispatchToProps = (dispatch) => ({
+	uploadImage: (path, filename) => dispatch(uploadImage(path, filename)),
+	getUploadedImages: () => dispatch(getUploadedImages())
+  });
+
+  const mapStateToProps = (state) => {
+	return {
+	  auth: state.auth,
+	  profile: state.profile
+	};
+};
+
 export default class App extends Component {
+  constructor(props){
+		super(props);
+		this.state = {
+			animating: true,
+			image: null,
+			image_name: undefined,
+			uploaded_img: null,
+		}
+		this.callUpload = this.callUpload.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange = e => {
+		if (e.target.files[0]) {
+		  const img = e.target.files[0];
+		  let name = img.name
+		  const filename = name.substring(0, img.name.indexOf('.'));
+		  this.setState(() => ({ image: img, image_name: filename }));
+		}
+	};
+
+	componentDidMount() {
+		if (this.props.profile.images === undefined || this.props.profile.images.length == 0) {
+			this.props.getUploadedImages()
+		}
+		this.props.profile.images.forEach((img) => {
+			console.log(img)
+			console.log(img.imageUrl)
+		});
+	}
+
+	componentDidUpdate(prevProps) {
+		if (this.props.profile.img !== prevProps.profile.img) {
+			if(this.props.profile.img ) {
+				this.setState({uploaded_img: this.props.profile.img})
+			}
+		}
+	}
+
+	callUpload = () => {
+		if (!this.state.image) {
+			alert("Select an image for upload.")
+			return
+		}
+		this.props.uploadImage(this.state.image, this.state.image_name)
+	}
+
+
   render() {
     return (
       <View style={styles.main}>
         <Image
-          source = {{
+          source={require('../img/nfty_logo.png')}
+          style={{
             width: 20,
             height: 20,
+            resizeMode: 'contain',
             alignSelf: 'right',
-            uri: "NftyProfile\assets\logo.png"
+            margin: 30,
           }}
         />
         <Text style={{fontSize: 20, fontWeight: "900", alignSelf:'left', marginBottom:20}}>Poppy Seda</Text>
@@ -28,14 +90,21 @@ export default class App extends Component {
         <Text style={{fontWeight: "bold", alignSelf:'left', marginBottom:20}}>My NFTs</Text>
 
         <View style={{flexDirection:'row', margin:20, justifyContent:"space-around"}}>
-        <Image
+        {this.props.profile.images.map((image) =>
+            <img
+            src={image.imageUrl}
+            height="200"
+            width="200"
+          />
+          )}
+        {/* <Image
           source = {{
             width: 200,
             height: 200,
             uri: "http://picsum.photos/200/300"
             //replace with image URL from the database
           }}
-        />
+        /> */}
         <Image
           source = {{
             width: 200,
@@ -54,14 +123,23 @@ export default class App extends Component {
         <Text style={{fontWeight: "bold", alignSelf:'left', marginBottom:20}}>My Stylized Images</Text>
 
         <View style={{flexDirection:'row', margin:20, justifyContent:"space-around"}}>
-          <Image
+          {this.props.profile.images.map((image) =>
+            <img
+            src={image.imageUrl}
+            height="200"
+            width="200"
+          />
+          )}
+          
+
+          {/* <Image
             source = {{
               width: 200,
               height: 200,
               uri: "http://picsum.photos/200/300"
               //replace with image URL from the database
             }}
-          />  
+          />   */}
           <Image
             source = {{
               width: 200,
