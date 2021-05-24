1a5771edcb93b49098cb433d16ec62309b23c0e3
Rachael Wang <rachaelwang2@gmail.com>
2021-05-23 20:21:13 -0700

    finish frontend for profile page

--- Nfty/src/views/screens/PersonalProfile.js
+++ Nfty/src/views/screens/PersonalProfile.js
@@ -1,288 +1,121 @@
 import React, { Component } from 'react';
-import {ActivityIndicator, View, StyleSheet, Image, Text, Button, TouchableOpacity} from 'react-native';
-import { connect } from "react-redux";
-import {globalStyle, AppStyles} from "./global-style";
-import {uploadImage, getUploadedImages} from "../../redux/actions/ActionCreators";
-import { Svg } from 'react-native-svg'
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {ActivityIndicator, SafeAreaView, View, StyleSheet, Image, Text, Button, TouchableOpacity} from 'react-native';
+// import { connect } from "react-redux";
+// import {globalStyle, AppStyles} from "./global-style";
+// import {uploadImage, getUploadedImages} from "../../redux/actions/ActionCreators";
+// import { Svg } from 'react-native-svg'
 
-export default function App(){
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text> Hello React Native</Text>
-      <Image
-        source = {{
-          width: 200,
-          height: 200,
-          uri: "http://picsum.photos/200/300"
-          //replace with image URL from the database
-        }}
-      />
-      <Image
-        source = {{
-          width: 200,
-          height: 200,
-          uri: "http://picsum.photos/200/300"
-          //replace with image URL from the database
-        }}
-      />
-      <Image
-        source = {{
-          width: 200,
-          height: 200,
-          uri: "http://picsum.photos/200/300"
-          //replace with image URL from the database
-        }}
-      />
-      <Image
-        source = {{
-          width: 200,
-          height: 200,
-          uri: "http://picsum.photos/200/300"
-          //replace with image URL from the database
-        }}
-      />
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.main}>
+        <Image
+          source = {{
+            width: 20,
+            height: 20,
+            alignSelf: 'right',
+            uri: "NftyProfile\assets\logo.png"
+          }}
+        />
+        <Text style={{fontSize: 20, fontWeight: "900", alignSelf:'left', marginBottom:20}}>Poppy Seda</Text>
 
-    </SafeAreaView>
+        <View
+          style={{
+          borderBottomColor: 'black',
+          borderBottomWidth: 1,
+          }}
+        />
+        <Text style={{fontWeight: "bold", alignSelf:'left', marginBottom:20}}>My NFTs</Text>
 
-  )
+        <View style={{flexDirection:'row', margin:20, justifyContent:"space-around"}}>
+        <Image
+          source = {{
+            width: 200,
+            height: 200,
+            uri: "http://picsum.photos/200/300"
+            //replace with image URL from the database
+          }}
+        />
+        <Image
+          source = {{
+            width: 200,
+            height: 200,
+            uri: "http://picsum.photos/200/300"
+            //replace with image URL from the database
+          }}
+        />
+        </View>
+        <View
+          style={{
+          borderBottomColor: 'black',
+          borderBottomWidth: 1,
+          }}
+        />
+        <Text style={{fontWeight: "bold", alignSelf:'left', marginBottom:20}}>My Stylized Images</Text>
 
+        <View style={{flexDirection:'row', margin:20, justifyContent:"space-around"}}>
+          <Image
+            source = {{
+              width: 200,
+              height: 200,
+              uri: "http://picsum.photos/200/300"
+              //replace with image URL from the database
+            }}
+          />  
+          <Image
+            source = {{
+              width: 200,
+              height: 200,
+              uri: "http://picsum.photos/200/300"
+              //replace with image URL from the database
+            }}
+          />
+        </View>
+        <Text>
+          <Text>{ this.addPadding() }</Text>
+          <Text>
+             <Text style={{fontWeight: "bold"}}></Text>{"\n"}
+          </Text>      
+        </Text>
+        <Text
+          style={{
+          borderBottomColor: 'black',
+          borderBottomWidth: 1,
+          }}
+        >Settings</Text>
+        <Text
+          style={{
+          borderBottomColor: 'black',
+          borderBottomWidth: 1,
+          }}
+        >About</Text>
+        <Text
+          style={{
+          borderBottomColor: 'black',
+          borderBottomWidth: 1,
+          }}
+        >Log out</Text>
+      </View>
 
+      
+    );
+  }
+
+addPadding() {
+    const padding = Array.apply(null, Array(16)).map(() => '').join('');
+    return Array.apply(null, Array(8)).map(() => padding + '\n');
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  main: {
     flex: 1,
-    backgroundColor: "#fff", 
-    justifyContent: "center", 
-    alignItems: "center"
+    margin: 40,
+  },
+
+  image: {
+    width: 200,
+    height: 200,
+    position: 'absolute',
   },
 });
-
-
-
-
-// export default function PersonalProfile () {
-//   return (
-//     <View style={Profile.FigmatoReact}>
-//       <Text style={Profile.Title}>
-//         Profile Page
-//       </Text>
-//       <Inputfield />
-//       <Button />
-//       <View style={Profile.TabMenu}>
-//         <TabButton />
-//         <TabButton />
-//         <TabButton />
-//       </View>
-//       <Code />
-//     </View>
-//   )
-// }
-
-// const Profile = StyleSheet.create({
-//   FigmatoReact: {
-//     backgroundColor: '#f9f9f9',
-//   },
-//   Title: {
-//     width: 643,
-//     height: 109,
-//     margin: '318 60',
-//     color: '#000000',
-//     fontFamily: 'Inter, sans-serif',
-//     fontSize: 90,
-//     fontWeight: '500',
-//     lineHeight: 105.46875,
-//     textAlign: 'center',
-//   },
-//   TabMenu: {
-//     width: 990,
-//     height: 84,
-//     margin: '144 517',
-//   },
-
-// })
-
-// function Inputfield () {
-//   return (
-//     <View style={InputfieldStyles.Inputfield}>
-//       <View style={InputfieldStyles.Shape} />
-//       <Text style={InputfieldStyles.Label}>
-//         Figma URL:
-//       </Text>
-//       <Text style={InputfieldStyles.Value}>
-//         https://www.figma.com/file/qosbLLy02hcWkJDEcodamN
-//       </Text>
-//     </View>
-//   )
-// }
-
-// const InputfieldStyles = StyleSheet.create({
-//   Inputfield: {
-//     width: 945,
-//     height: 83,
-//     margin: '167 213',
-//   },
-//   Shape: {
-//     width: '76.08%',
-//     height: '100%',
-//     borderRadius: 8,
-//     margin: '226 0',
-//     backgroundColor: '#ffffff',
-//     borderColor: '#888888',
-//     borderWidth: 2,
-//   },
-//   Label: {
-//     width: 216,
-//     height: 48,
-//     color: '#000000',
-//     fontFamily: 'Inter, sans-serif',
-//     fontSize: 40,
-//     fontWeight: '500',
-//     lineHeight: 46.875,
-//     textAlign: 'right',
-//   },
-//   Value: {
-//     width: '115.34%',
-//     height: '57.83%',
-//     margin: '246 17',
-//     color: '#000000',
-//     fontFamily: 'Inter, sans-serif',
-//     fontSize: 40,
-//     fontWeight: '500',
-//     lineHeight: 46.875,
-//   },
-
-// })
-
-// function Generate_button () {
-//   return (
-//     <View style={ButtonStyles.Button}>
-//       <Text style={ButtonStyles.Label}>
-//         Generate
-//       </Text>
-//     </View>
-//   )
-// }
-
-// const ButtonStyles = StyleSheet.create({
-//   Button: {
-//     width: 450,
-//     height: 120,
-//     borderRadius: 16,
-//     margin: '417 321',
-//     backgroundColor: '#2d9cdb',
-//   },
-//   Label: {
-//     width: '40%',
-//     height: '40%',
-//     margin: '135 36',
-//     color: '#ffffff',
-//     fontFamily: 'Inter, sans-serif',
-//     fontSize: 40,
-//     lineHeight: 46.875,
-//     textAlign: 'center',
-//     textAlignVertical: 'center',
-//   },
-
-// })
-
-// function Code () {
-//   return (
-//     <View style={CodeStyles.Code}>
-//       <View style={CodeStyles.Rectangle1} />
-//       <Text style={CodeStyles.Outputcode}>
-//         Text
-//       </Text>
-//     </View>
-//   )
-// }
-
-// const CodeStyles = StyleSheet.create({
-//   Code: {
-//     width: 990,
-//     height: 422,
-//     margin: '144 601',
-//   },
-//   Rectangle1: {
-//     width: '100%',
-//     height: '100%',
-//     borderRadius: 8,
-//     backgroundColor: '#ffffff',
-//     borderColor: '#dddddd',
-//     borderWidth: 1,
-//   },
-//   Outputcode: {
-//     width: '95.56%',
-//     height: '6.87%',
-//     margin: '23 24',
-//     color: '#000000',
-//     fontFamily: 'Inter, sans-serif',
-//     fontSize: 24,
-//     lineHeight: 28.125,
-//   },
-
-// })
-
-// function TabButton () {
-//   return (
-//     <View style={TabButtonStyles.TabButton}>
-//       <View style={TabButtonStyles.Shape} />
-//       <Text style={TabButtonStyles.Label}>
-//         React Web
-//       </Text>
-//     </View>
-//   )
-// }
-
-// const TabButtonStyles = StyleSheet.create({
-//   TabButton: {
-//     width: 330,
-//     height: 84,
-//   },
-//   Shape: {
-//     width: '100%',
-//     height: '100%',
-//     borderRadius: 8,
-//     backgroundColor: '#eeeeee',
-//     borderColor: '#000000',
-//     borderWidth: 2,
-//   },
-//   Label: {
-//     width: '100%',
-//     height: '57.14%',
-//     color: '#000000',
-//     fontFamily: 'Inter, sans-serif',
-//     fontSize: 40,
-//     lineHeight: 46.875,
-//     textAlign: 'center',
-//     textAlignVertical: 'center',
-//   },
-
-// })
-
-
-
-
-
-
-// export default function App() {
-//   return (
-//     <SafeAreaView style={styles.container}>
-//       <Text numberOfLines={1} on>
-
-//       </Text>
-
-//     </SafeAreaView>
-
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1, 
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center"
-//   },
-// }); 
